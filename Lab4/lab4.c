45d0bfe8f606d1ef24714e9f701f0ebd333bb03c
eye <arham.samir01@gmail.com>
2023-11-14 03:09:30 -0600

    Update lab4.c

--- Lab4/lab4.c
+++ Lab4/lab4.c
@@ -126,11 +126,16 @@ void printMaxLength(struct Queue* queue) {
         printf("Error: Queue is empty\n");
     }
 }
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
     struct Queue myQueue;
     initQueue(&myQueue);
 
-    FILE *file = fopen("a.dat", "r");
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
